337fb66bb50f16a4c7b8d2fa6fb16586b32d8161
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-04-04 22:46:17 +0200

    styled News Section

--- src/components/organisms/SearchBar/SearchBar.styles.js
+++ src/components/organisms/SearchBar/SearchBar.styles.js
@@ -27,9 +27,3 @@ export const StatusInfo = styled.div`
     margin: 5px;
   }
 `;
-
-export const News = styled.div`
-  grid-row: 1 / 3;
-  grid-column: 3 / 3;
-  border-left: 1px solid ${({ theme }) => theme.colors.darkPurple};
-`;
--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -1,27 +1,52 @@
 import { Button } from 'components/atoms/Button/Button';
-import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
 import React from 'react';
-import { Wrapper } from './NewsSection.styles';
+import {
+  ArticleWrapper,
+  NewsSectionHeader,
+  Wrapper,
+  TitleWrapper,
+  ContentWrapper,
+} from './NewsSection.styles';
+
+const data = [
+  {
+    title: 'New computer at school',
+    category: 'Tech news',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    title: 'New computer at school2',
+    category: 'Tech news',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: 'https://unsplash.it/500/400',
+  },
+  {
+    title: 'New computer at school3',
+    category: 'Tech news',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+];
 
 const NewsSection = () => {
   return (
     <Wrapper>
-      <h2>University news feed</h2>
-      <ViewWrapper>
-        <h3>Lorem ipsum</h3>
-        <p>dolor sit amet</p>
-        <Button>click me</Button>
-      </ViewWrapper>
-      <ViewWrapper>
-        <h3>Lorem ipsum</h3>
-        <p>dolor sit amet</p>
-        <Button>click me</Button>
-      </ViewWrapper>
-      <ViewWrapper>
-        <h3>Lorem ipsum</h3>
-        <p>dolor sit amet</p>
-        <Button>click me</Button>
-      </ViewWrapper>
+      <NewsSectionHeader>University news feed</NewsSectionHeader>
+      {data.map(({ title, category, content, image = null }) => (
+        <ArticleWrapper key={title}>
+          <TitleWrapper>
+            <h3>{title}</h3>
+            <p>{category}</p>
+          </TitleWrapper>
+          <ContentWrapper>
+            <p>{content}</p>
+            {image ? <img src={image} alt="article image" /> : null}
+          </ContentWrapper>
+          <Button isBig>Read mode</Button>
+        </ArticleWrapper>
+      ))}
     </Wrapper>
   );
 };
